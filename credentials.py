7a7131527f18a5030147af82aadc488ed38d96d2
Ishan Gupta <ishangupta@Ishans-MBP.attlocal.net>
2024-07-13 16:29:24 -0400

    Added sleep timer

--- credentials.py
+++ credentials.py
@@ -1,6 +1,6 @@
 #  this is just  a sample config file.You need to edit this file.You need to get these details from your Twitter app settings.
 
-consumer_key = 'your_consumer_key'
-consumer_secret = 'your_consumer_secret'
-access_token = 'your_access_token'
-access_token_secret = 'your_access_token_secret'
+consumer_key = "JaU19Solkhkt35qkfS7DSXQgr"
+consumer_secret = "fXMCGm8zoRItpQ873uUR27hVMhgIwoDJuwqE7pou4VKiVz6KnQ"
+access_token = "1490466644-G7JXOguk2xpTUhxDbYovjf8KFZhmoWkVw745wlR"
+access_token_secret = "3oGbX0GrrBovvmVm6VVc7pcQeQVbZYZKa0UZJGZCjBjUj"
--- twitterbot_text.py
+++ twitterbot_text.py
@@ -4,27 +4,44 @@ from time import sleep
 # Import Twitter credentials from credentials.py
 from credentials import *
 
+    
+client = tweepy.Client(
+    consumer_key=consumer_key,
+    consumer_secret=consumer_secret,
+    access_token=access_token,
+    access_token_secret=access_token_secret
+)
 
-auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
-auth.set_access_token(access_token, access_token_secret)
+auth = tweepy.OAuth1UserHandler(consumer_key, consumer_secret)
+auth.set_access_token(
+    access_token,
+    access_token_secret,
+)
 api = tweepy.API(auth)
-
 my_file=open('sample.txt','r')
 file_lines=my_file.readlines()
 my_file.close()
 
-# Create a for loop to iterate over file_lines
-for line in file_lines:
-# Add try ... except block to catch and output errors
-    try:
-        print(line)
-        # Add if statement to ensure that blank lines are skipped
-        if line != '\n':
-            api.update_status(line)
+def tweet_text():
+    for line in file_lines:
+        try:
+            print(line)
+            if line != "\n":
+                client.create_tweet(text=line)
+            else:
+                pass
+        except tweepy.TweepError as e:
+            print(e.reason)
 
-        # Add an else statement with pass to conclude the conditional statement
-        else:
-            pass
+def tweet_with_image():
+    try:
+        media_path = "./twitter_text_bot.png"
+        media = api.media_upload(filename=media_path)
+        media_id = media.media_id
+        client.create_tweet(text="Tweet text", media_ids=[media_id])
     except tweepy.TweepError as e:
         print(e.reason)
-    sleep(10)
+
+tweet_with_image()
+
+sleep(10)
